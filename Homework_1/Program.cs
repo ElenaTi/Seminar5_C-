cd394c5e33e24e9e64e9c88c07b0173361cd7c10
Elena Timofeeva <posazho@gmail.com>
2022-07-08 21:32:14 +0300

    Количество четных чисел в массиве

---
+++ Homework_1/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] FillArray (int length, int beginBorder, int endBorder)
+{
+    int[] ResultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        ResultArray[i] = new Random().Next(beginBorder, endBorder);
+    }
+    return ResultArray;
+}
+
+void ShowArray(int[] arrayToShow)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToShow.Length; i++)
+    {
+        Console.Write(arrayToShow[i]);
+        if (i < arrayToShow.Length-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int QuantityOfEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {       
+        if (array[i] % 2 == 0)
+        {
+            count++;
+            
+        }
+    }
+    return count;
+}
+
+int[] NewArray = FillArray(10, 100, 1000);
+ShowArray(NewArray);
+Console.WriteLine("");
+int evenNumbers = QuantityOfEvenNumbers(NewArray);
+Console.WriteLine(evenNumbers);
